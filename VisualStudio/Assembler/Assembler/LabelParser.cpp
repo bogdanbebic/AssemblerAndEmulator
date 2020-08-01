c6bfa981c79d5397321f1ba571426c9221ff124c
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-08-01 15:37:15 +0200

    Added TODO comment

--- VisualStudio/Assembler/Assembler/LabelParser.cpp
+++ VisualStudio/Assembler/Assembler/LabelParser.cpp
@@ -18,9 +18,10 @@ std::string parsers::LabelParser::parse(std::string line, size_t section_index, 
 		if (std::regex_match(line.c_str(), this->match_, this->regex_))
 		{
 			std::cout << "LABEL:'" << this->match_[1].str() << "'\n";
-			// TODO: add label to symbol table
 			std::string symbol = this->match_[1].str();
 			assembler::SymbolTable::SymbolTableEntry entry = { symbol, line_counter, section_index, false };
+
+			// TODO: check if already exists - maybe throw an exception
 			this->symbol_table_->insert({ symbol, entry });
 			
 			return this->match_[2].str();
