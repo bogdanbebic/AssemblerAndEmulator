fe46734e917bb1893ed6cf0a18106d960e91d280
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-08-08 18:46:24 +0200

    Added .section directive parsing
    TODO: implement section size update on section end

--- VisualStudio/Assembler/Assembler/AssemblyDirectiveParser.cpp
+++ VisualStudio/Assembler/Assembler/AssemblyDirectiveParser.cpp
@@ -21,11 +21,11 @@ std::shared_ptr<statements::Statement> parsers::AssemblyDirectiveParser::parse(s
 			return std::make_shared<statements::Statement>(0, true);
 		}
 		
-		const std::regex regex{ "^\\.(global|extern|section|equ)\\s*(.*)$" };
+		const std::regex regex{R"(^\.(global|extern|section|equ)\s*(.*)$)"};
 		const std::regex symbol_list_regex{
-			"^([a-zA-Z_][a-zA-Z_0-9]*)\\s*(,\\s*([a-zA-Z_][a-zA-Z_0-9]*))*$"
+			R"(^([a-zA-Z_][a-zA-Z_0-9]*)\s*(,\s*([a-zA-Z_][a-zA-Z_0-9]*))*\s*$)"
 		};
-		const std::regex section_name_regex{ "^([a-zA-Z_][a-zA-Z_0-9]*)$" };
+		const std::regex section_name_regex{ "^([a-zA-Z_][a-zA-Z_0-9]*)\\s*$" };
 
 		std::smatch match;
 		if (std::regex_match(statement, match, regex))
@@ -33,10 +33,39 @@ std::shared_ptr<statements::Statement> parsers::AssemblyDirectiveParser::parse(s
 			// extract directive
 			std::string directive = match[1];
 			std::string args = match[2];
+
+			// TODO: delete
 			std::cout << "directive: '" << directive << "'\n"
 				<< "args: '" << args << "'\n";
 
-			// TODO: implement
+			if (directive == "section")
+			{
+				if (!std::regex_match(args, match, section_name_regex))
+				{
+					throw std::invalid_argument{ "Invalid args for .section directive" };
+				}
+
+				std::string section_name = match[1];
+				this->section_table_->insert(section_name);
+			}
+
+			if (directive == "global")
+			{
+				// TODO: implement
+				
+			}
+
+			if (directive == "extern")
+			{
+				// TODO: implement
+				
+			}
+
+			if (directive == "equ")
+			{
+				// TODO: implement
+				
+			}
 		}
 		
 		return nullptr;
