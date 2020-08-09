e2c943e3e5a435b636850af4a30a121f3a1a3949
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-08-09 20:43:09 +0200

    Added typedefs for byte and word

---
+++ VisualStudio/Assembler/Assembler/DataDefs.h
@@ -0,0 +1,13 @@
+#ifndef DATA_DEFS_H_GUARD
+// ReSharper disable once IdentifierTypo
+#define DATA_DEFS_H_GUARD
+
+#include <cstdint>
+
+namespace assembler
+{
+	typedef uint8_t byte;
+	typedef uint16_t word;
+}
+
+#endif
