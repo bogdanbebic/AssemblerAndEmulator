39fb29e6c3b0d37fe19fe1a4a84631904bb81497
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-09-19 16:53:08 +0200

    emulator: Add constexpr's for max values of mem_addr, byte, word

--- VisualStudio/Emulator/Emulator/Typedefs.h
+++ VisualStudio/Emulator/Emulator/Typedefs.h
@@ -10,6 +10,10 @@ namespace emulator
 		using mem_address_t = uint16_t;
 		using byte_t = uint8_t;
 		using word_t = uint16_t;
+
+		constexpr mem_address_t mem_address_max = UINT16_MAX;
+		constexpr byte_t byte_max = UINT8_MAX;
+		constexpr word_t word_max = UINT16_MAX;
 	}
 }
 
