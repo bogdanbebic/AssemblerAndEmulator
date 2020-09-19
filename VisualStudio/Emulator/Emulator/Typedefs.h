c75521ceb70a86cb7654e4e2953bbf5546519a16
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-09-19 16:44:45 +0200

    emulator: Add alias defs for mem_address, byte, word

---
+++ VisualStudio/Emulator/Emulator/Typedefs.h
@@ -0,0 +1,16 @@
+#ifndef _TYPEDEFS_H_
+#define _TYPEDEFS_H_
+
+#include <cstdint>
+
+namespace emulator
+{
+	namespace system
+	{
+		using mem_address_t = uint16_t;
+		using byte_t = uint8_t;
+		using word_t = uint16_t;
+	}
+}
+
+#endif
