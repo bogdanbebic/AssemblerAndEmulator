a3849b50662e103bbaf803196eb4ce16684ee224
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-09-26 16:19:27 +0200

    timer: Implement skeleton timer device
    The Timer class represents a Timer hardware device 
    and as such it can not be copied or moved. It has
    a thread of execution for itself - it sleeps for a
    period of time configured by the timer_cfg reg.
    
    The timer will generate interrupts to the CPU on
    every timeout - the timeout happens every time the
    timer thread wakes up.
    
    The Timer is a RAII class - it's thread starts upon
    construction - and is joined upon destruction. The
    destruction might generate a delay in program
    execution based on the current configured value
    in the timer_cfg register.

--- VisualStudio/Emulator/Emulator/Timer.cpp
+++ VisualStudio/Emulator/Emulator/Timer.cpp
@@ -1,1 +1,36 @@
 #include "Timer.h"
+
+std::map<emulator::system::word_t, std::chrono::milliseconds>
+emulator::system::Timer::timeouts_ = {
+	{ 0x0, std::chrono::milliseconds{ 500 } },
+	{ 0x1, std::chrono::milliseconds{ 1000 } },
+	{ 0x2, std::chrono::milliseconds{ 1500 } },
+	{ 0x3, std::chrono::milliseconds{ 2000 } },
+	{ 0x4, std::chrono::milliseconds{ 5000 } },
+	{ 0x5, std::chrono::seconds{ 10 } },
+	{ 0x6, std::chrono::seconds{ 30 } },
+	{ 0x7, std::chrono::seconds{ 60 } },
+};
+
+void emulator::system::Timer::set_timer_cfg(const word_t timer_cfg)
+{
+	this->timer_cfg_ = timer_cfg;
+
+	this->timeout_ = timeouts_[this->timer_cfg_ & timer_cfg_timeout_mask];
+}
+
+emulator::system::Timer::~Timer()
+{
+	this->running_ = false;
+	this->timer_thread_.join();
+}
+
+void emulator::system::Timer::timer() const
+{
+	while (this->running_)
+	{
+		// TODO: send interrupt to CPU
+
+		std::this_thread::sleep_for(this->timeout_);
+	}
+}
--- VisualStudio/Emulator/Emulator/Timer.h
+++ VisualStudio/Emulator/Emulator/Timer.h
@@ -1,13 +1,41 @@
 #ifndef _TIMER_H_
 #define _TIMER_H_
 
+#include <chrono>
+#include <map>
+#include <thread>
+
+#include "Typedefs.h"
+
 namespace emulator
 {
 	namespace system
 	{
 		class Timer
 		{
-		};		
+		public:
+			void set_timer_cfg(word_t timer_cfg);
+
+			Timer() = default;
+
+			Timer(const Timer&) = delete;
+			Timer(Timer&&) = delete;
+			void operator= (const Timer&) = delete;
+			void operator= (Timer&&) = delete;
+			
+			~Timer();
+		private:
+			void timer() const;
+
+			static std::map<word_t, std::chrono::milliseconds> timeouts_;
+
+			static constexpr word_t timer_cfg_timeout_mask = 0x7;
+			word_t timer_cfg_ = 0x0;
+			std::chrono::milliseconds timeout_ = timeouts_[timer_cfg_ & timer_cfg_timeout_mask];
+
+			bool running_ = true;
+			std::thread timer_thread_{ &Timer::timer, this };
+		};
 	}
 }
 
