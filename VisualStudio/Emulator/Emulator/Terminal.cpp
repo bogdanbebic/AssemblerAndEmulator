16c6f1229f503de23a7f30a21689eb7ba85a4297
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-09-26 20:59:39 +0200

    terminal: Implement skeleton terminal device
    The Terminal class represents a Terminal hardware
    device and as such it can not be copied or moved. It
    has a thread of execution for itself - it reads user
    input and keeps the last read byte in it's data_in
    register (and will raise an interrupt to the CPU when
    it has read user input). When writing to the data_out
    register - Terminal prints the char written to the
    output.
    
    The Terminal is a RAII class - it's thread starts upon
    construction - and is joined upon destruction.
    The destruction might generate a wait for user input
    on Win32 as Terminal blocks on user input.

--- VisualStudio/Emulator/Emulator/Terminal.cpp
+++ VisualStudio/Emulator/Emulator/Terminal.cpp
@@ -1,1 +1,77 @@
 #include "Terminal.h"
+
+#include <iostream>
+
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+
+void emulator::system::Terminal::set_data_out(const word_t data_out)
+{
+	this->data_out_ = data_out & data_out_mask;
+
+	// write data_out to terminal
+	std::cout << static_cast<char>(this->data_out_);
+}
+
+emulator::system::word_t emulator::system::Terminal::data_in() const
+{
+	return this->data_in_;
+}
+
+emulator::system::Terminal::Terminal()
+{
+	this->enter_raw_mode();
+}
+
+emulator::system::Terminal::~Terminal()
+{
+	this->running_ = false;
+	this->terminal_thread_.join();
+	this->exit_raw_mode();
+}
+
+void emulator::system::Terminal::terminal()
+{
+	while (this->running_)
+	{
+		char ch;
+
+		// read one char
+#ifdef _WIN32
+		std::cin >> ch;
+#else
+		// Linux
+		if (read(STDIN_FILENO, &ch, 1) != 1)
+		{
+			continue;
+		}
+#endif
+
+		this->data_in_ = ch & data_in_mask;
+
+		// TODO: send interrupt to CPU
+	}
+}
+
+void emulator::system::Terminal::enter_raw_mode()
+{
+#ifndef _WIN32
+	// Linux
+	struct termios raw;
+	tcgetattr(STDIN_FILENO, &raw);
+	this->old_termios = raw;
+
+	raw.c_lflag &= ~ICANON;
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+#endif
+}
+
+void emulator::system::Terminal::exit_raw_mode()
+{
+#ifndef _WIN32
+	// Linux
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &this->old_termios);
+#endif
+}
--- VisualStudio/Emulator/Emulator/Terminal.h
+++ VisualStudio/Emulator/Emulator/Terminal.h
@@ -1,12 +1,49 @@
 #ifndef _TERMINAL_H_
 #define _TERMINAL_H_
 
+#include <thread>
+
+#ifndef _WIN32
+#include <termios.h>
+#endif
+
+#include "Typedefs.h"
+
 namespace emulator
 {
 	namespace system
 	{
 		class Terminal
 		{
+		public:
+			void set_data_out(word_t data_out);
+			word_t data_in() const;
+
+			Terminal();
+
+			Terminal(const Terminal&) = delete;
+			Terminal(Terminal&&) = delete;
+			void operator= (const Terminal&) = delete;
+			void operator= (Terminal&&) = delete;
+			
+			~Terminal();
+		private:
+			void terminal();
+
+			void enter_raw_mode();
+			void exit_raw_mode();
+
+			#ifndef _WIN32
+			struct termios old_termios;
+			#endif
+
+			static constexpr word_t data_out_mask = 0xFF;
+			static constexpr word_t data_in_mask  = 0xFF;
+			word_t data_out_ = 0;
+			word_t data_in_  = 0;
+			
+			bool running_ = true;
+			std::thread terminal_thread_{ &Terminal::terminal, this };
 		};
 	}	
 }
