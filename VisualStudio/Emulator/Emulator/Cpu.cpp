c81d3787aedc0368073724d6d3600dc07c7d8b54
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-10-06 17:18:43 +0200

    cpu: Implement structure and interrupt interface
    The Cpu class represents a physical CPU of the
    emulator. The structure contains the registers of
    the CPU - GP regs, memory mapped regs and the
    PSW along with all necessary definitions.
    
    The instructions structs represent an abstraction
    for CPU instructions to be executed - implemented
    along with all the necessary definitions regarding
    opcodes, addressing modes, descriptors, etc.
    
    The interrupt interface is simple - just one skeleton
    function, along with interrupts constants definitions.
    We need this for hardware interrupts as well as
    software interrupts in the emulation.

--- VisualStudio/Emulator/Emulator/Cpu.cpp
+++ VisualStudio/Emulator/Emulator/Cpu.cpp
@@ -1,1 +1,6 @@
 #include "Cpu.h"
+
+void emulator::system::cpu::Cpu::interrupt(const size_t ivt_entry)
+{
+	// TODO: call handler for interrupt
+}
---
+++ VisualStudio/Emulator/Emulator/CpuDefs.h
@@ -0,0 +1,48 @@
+#ifndef _CPU_DEFS_H_
+#define _CPU_DEFS_H_
+
+#include "Typedefs.h"
+
+namespace emulator
+{
+	namespace system
+	{
+		namespace cpu
+		{
+			constexpr size_t size_memory_mapped_registers = 256;
+
+			constexpr size_t num_gp_registers = 8;
+			enum GpRegisters
+			{
+				REG_PC = 6,
+				REG_SP = 7,
+			};
+
+			constexpr size_t ivt_num_entries = 8;
+			enum IvtEntries
+			{
+				IVT_PROC_START = 0,
+				IVT_INVALID_OP = 1,
+				IVT_TIMER = 2,
+				IVT_TERMINAL = 3,
+				/* others free for use */
+			};
+
+			using ivt_entry_t = word_t;
+			
+			enum class PswMasks
+			{
+				PSW_Z_MASK	= 1 << 0,
+				PSW_O_MASK	= 1 << 1,
+				PSW_C_MASK	= 1 << 2,
+				PSW_N_MASK	= 1 << 3,
+				/* free space */
+				PSW_TR_MASK = 1 << 13,
+				PSW_TL_MASK = 1 << 14,
+				PSW_I_MASK	= 1 << 15,
+			};
+		}
+	}
+}
+
+#endif
