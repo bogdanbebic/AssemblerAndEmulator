8be5e6b832b8dea18a2b967987dbfa5abf76b896
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-10-06 17:18:43 +0200

    cpu: Implement structure and interrupt interface
    The Cpu class represents a physical CPU of the
    emulator. The structure contains the registers of
    the CPU - GP regs, memory mapped regs and the
    PSW along with all necessary definitions.
    
    The instructions structs represent an abstraction
    for CPU instructions to be executed - implemented
    along with all the necessary definitions regarding
    opcodes, addressing modes, descriptors, etc.
    
    The interrupt interface is simple - just one skeleton
    function, along with interrupts constants definitions.
    We need this for hardware interrupts as well as
    software interrupts in the emulation.

--- VisualStudio/Emulator/Emulator/Cpu.h
+++ VisualStudio/Emulator/Emulator/Cpu.h
@@ -1,13 +1,29 @@
 #ifndef _CPU_H_
 #define _CPU_H_
 
+#include "CpuDefs.h"
+#include "Typedefs.h"
+#include "Psw.h"
+
 namespace emulator
 {
 	namespace system
 	{
-		class Cpu
+		namespace cpu
 		{
-		};
+			class Cpu
+			{
+			public:
+				void interrupt(size_t ivt_entry);
+			private:
+				byte_t memory_mapped_registers_[size_memory_mapped_registers] = { 0, };
+				word_t general_purpose_registers_[num_gp_registers] = { 0, };
+			
+				const mem_address_t interrupt_vector_table_pointer_ = 0;
+
+				Psw psw_;
+			};
+		}
 	}
 }
 
---
+++ VisualStudio/Emulator/Emulator/Psw.h
@@ -0,0 +1,28 @@
+#ifndef _PSW_H_
+#define _PSW_H_
+
+#include "CpuDefs.h"
+#include "Typedefs.h"
+
+namespace emulator
+{
+	namespace system
+	{
+		namespace cpu
+		{
+			class Psw
+			{
+			public:
+				bool psw_read(PswMasks bit) const;
+				void psw_write(PswMasks bit, bool value);
+			private:
+				void psw_clear(PswMasks bit);
+				void psw_set(PswMasks bit);
+
+				word_t psw_ = 0;
+			};
+		}
+	}
+}
+
+#endif
