f675dfcf9bbe0c40e5bdde7886ccda114748d30e
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-10-10 12:54:55 +0200

    cmd-opts-parser: Remove logging statements in main

--- VisualStudio/Emulator/Emulator/main.cpp
+++ VisualStudio/Emulator/Emulator/main.cpp
@@ -15,20 +15,6 @@ int main(int argc, char* argv[])
 		std::cout << emulator::utility::CommandLineOptionsParser::help_msg();
 		return 0;
 	}
-	
-	std::cout << "-h = --help   =" << cmd_parser.is_help_option() << std::endl;
-	std::cout << "--bare-memory =" << cmd_parser.is_bare_memory_option() << std::endl;
-	std::cout << "--place args:\n";
-	for (auto elem : cmd_parser.section_address_map())
-	{
-		std::cout << "section = '" << elem.first << "' @ address = '" << elem.second << std::endl;
-	}
-
-	std::cout << "source file paths:\n";
-	for (auto elem : cmd_parser.source_file_paths())
-	{
-		std::cout << "source file: " << elem << std::endl;
-	}
 
 	if (cmd_parser.is_help_option())
 	{
