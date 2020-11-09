7d7ada78f575fb81975265ab970287520faeb613
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-11-09 12:46:17 +0100

    assembler/data_def: Add .word and .byte parsing

--- VisualStudio/Assembler/Assembler/DataDefinitionParser.cpp
+++ VisualStudio/Assembler/Assembler/DataDefinitionParser.cpp
@@ -2,6 +2,7 @@
 
 #include <regex>
 #include <stdexcept>
+#include <sstream>
 
 #include "LiteralParser.h"
 
@@ -19,7 +20,8 @@ std::shared_ptr<statements::Statement> parsers::DataDefinitionParser::parse(std:
 	{
 		std::cout << "DATA_DEF: '" << statement << "'\n";
 		const std::regex data_skip_regex{R"(^\.skip\s+([^\s]+)\s*$)"};
-		// const std::regex data_def_regex{ "^\\.(byte|word)\\s+()" };
+		const std::regex data_byte_regex{R"(^\.byte\s+(.+)$)"};
+		const std::regex data_word_regex{R"(^\.word\s+(.+)$)"};
 		std::smatch match;
 		
 		if (std::regex_match(statement, match, data_skip_regex))
@@ -35,6 +37,30 @@ std::shared_ptr<statements::Statement> parsers::DataDefinitionParser::parse(std:
 			else
 			{
 				throw std::invalid_argument{ "Invalid argument for skip directive" };
+			}
+		}
+
+		if (std::regex_match(statement, match, data_byte_regex))
+		{
+			std::stringstream ss(match[1].str());
+			std::string byte_def;
+
+			while (std::getline(ss, byte_def, ','))
+			{
+				std::cout << byte_def << std::endl;
+				// TODO: implement
+			}
+		}
+
+		if (std::regex_match(statement, match, data_word_regex))
+		{
+			std::stringstream ss(match[1].str());
+			std::string word_def;
+
+			while (std::getline(ss, word_def, ','))
+			{
+				std::cout << word_def << std::endl;
+				// TODO: implement
 			}
 		}
 	}
