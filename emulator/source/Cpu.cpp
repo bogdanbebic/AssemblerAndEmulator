ed5015b68c64afb9d26e323de59f7cb0404bb25a
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-11-16 18:04:29 +0100

    emulator/cpu: Move definitions to be in order matching the class

--- emulator/source/Cpu.cpp
+++ emulator/source/Cpu.cpp
@@ -11,6 +11,25 @@ emulator::system::cpu::Cpu::Cpu(std::shared_ptr<Memory> memory)
 	// empty body
 }
 
+void emulator::system::cpu::Cpu::interrupt(const size_t ivt_entry)
+{
+	if (ivt_entry >= ivt_num_entries)
+		throw std::invalid_argument{ "Usage fault: invalid ivt_entry" };
+	this->interrupt_pending_[ivt_entry] = true;
+}
+
+void emulator::system::cpu::Cpu::work()
+{
+	this->cpu_running_ = true;
+	this->general_purpose_registers_[REG_PC] = this->memory_->read_word(this->interrupt_vector_table_pointer_);
+	while (this->cpu_running_)
+	{
+		instruction::instruction_t instr = this->fetch_instruction();
+		this->execute_instruction(instr);
+		this->handle_interrupt();
+	}
+}
+
 emulator::system::cpu::instruction::instruction_t emulator::system::cpu::Cpu::fetch_instruction()
 {
 	using namespace instruction;
@@ -51,25 +70,6 @@ emulator::system::cpu::instruction::instruction_t emulator::system::cpu::Cpu::fe
 void emulator::system::cpu::Cpu::execute_instruction(instruction::instruction_t instr)
 {
 	// TODO: implement
-}
-
-void emulator::system::cpu::Cpu::interrupt(const size_t ivt_entry)
-{
-	if (ivt_entry >= ivt_num_entries)
-		throw std::invalid_argument{ "Usage fault: invalid ivt_entry" };
-	this->interrupt_pending_[ivt_entry] = true;
-}
-
-void emulator::system::cpu::Cpu::work()
-{
-	this->cpu_running_ = true;
-	this->general_purpose_registers_[REG_PC] = this->memory_->read_word(this->interrupt_vector_table_pointer_);
-	while (this->cpu_running_)
-	{
-		instruction::instruction_t instr = this->fetch_instruction();
-		this->execute_instruction(instr);
-		this->handle_interrupt();
-	}
 }
 
 void emulator::system::cpu::Cpu::handle_interrupt()
