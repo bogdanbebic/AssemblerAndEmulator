168a3e8113d7fe4ac1e2e496353e8f43c6b5d2c2
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-11-18 16:08:36 +0100

    scripts: Add format convenience scripts

---
+++ scripts/format_as.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ROOT_DIR=$(realpath `git rev-parse --git-dir`/..)
+
+AS_DIR=$ROOT_DIR/assembler
+
+echo format assembler
+pushd $ROOT_DIR > /dev/null
+clang-format -i $AS_DIR/source/* $AS_DIR/include/*
+popd > /dev/null
