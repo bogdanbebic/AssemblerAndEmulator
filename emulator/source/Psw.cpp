2f651d4345e35cf213d21525b0458eb41bf9345d
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-11-19 18:07:42 +0100

    emulator/psw: Add get and set member functions

--- emulator/source/Psw.cpp
+++ emulator/source/Psw.cpp
@@ -13,6 +13,16 @@ void emulator::system::cpu::Psw::psw_write(const PswMasks bit, const bool value)
         this->psw_clear(bit);
 }
 
+emulator::system::word_t emulator::system::cpu::Psw::get() const
+{
+    return this->psw_;
+}
+
+void emulator::system::cpu::Psw::set(word_t psw)
+{
+    this->psw_ = psw;
+}
+
 void emulator::system::cpu::Psw::psw_clear(const PswMasks bit)
 {
     this->psw_ &= ~static_cast<word_t>(bit);
