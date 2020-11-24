48ae2e89f6cfcd35773042a3381f14f8c84388fb
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-11-24 17:17:59 +0100

    assembler: Implement ImmediateParser

---
+++ assembler/include/ImmediateParser.hpp
@@ -0,0 +1,26 @@
+#ifndef _IMMEDIATE_PARSER_HPP_
+#define _IMMEDIATE_PARSER_HPP_
+
+#include "OperandParser.hpp"
+
+#include "SymbolTable.hpp"
+
+namespace parsers
+{
+    class ImmediateParser final : public OperandParser
+    {
+    public:
+        std::shared_ptr<statement::operand_t> parse(std::string operand) override;
+        std::shared_ptr<statement::operand_t> parse_jump_instruction(std::string operand) override;
+
+        explicit ImmediateParser(std::shared_ptr<assembler::SymbolTable> symbol_table);
+
+    private:
+        bool can_parse(const std::string &operand) const;
+        bool can_parse_jump_instruction(const std::string &operand) const;
+
+        std::shared_ptr<assembler::SymbolTable> symbol_table_;
+    };
+} // namespace parsers
+
+#endif
