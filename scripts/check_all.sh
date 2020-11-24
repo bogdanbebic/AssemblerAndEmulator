a0f48d70399ce7b5be3a22b74317b5c43a741365
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-11-24 12:25:34 +0100

    scripts: Add check_all.sh convenience script

---
+++ scripts/check_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ROOT_DIR=$(realpath `git rev-parse --git-dir`/..)
+
+pushd $ROOT_DIR > /dev/null
+
+./scripts/format.sh
+./scripts/unit_test_as.sh
+./scripts/make.sh
+./scripts/run_behave_tests.sh -o /dev/null
+
+popd > /dev/null
