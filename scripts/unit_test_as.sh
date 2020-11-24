0702eb65c50593364c824e0909c4fd55000cc12c
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-11-24 12:24:55 +0100

    scripts: Add quotes on echos in unit_test_as.sh

--- scripts/unit_test_as.sh
+++ scripts/unit_test_as.sh
@@ -2,10 +2,10 @@
 
 AS_DIR=$(realpath `git rev-parse --git-dir`/..)/assembler
 
-echo build assembler
+echo "build assembler"
 pushd $AS_DIR > /dev/null
 make test
-echo assembler build successfull
+echo "assembler build successfull"
 popd > /dev/null
-
+echo "unit test assembler"
 $AS_DIR/build/test_assembler "$@"
