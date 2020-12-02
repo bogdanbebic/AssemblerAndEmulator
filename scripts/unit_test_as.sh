38960c710fb6792634a3a985b65239d047a00daf
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-12-02 13:17:52 +0100

    scripts/unit_test_as: Add exit code for script to represent success
    This enables scripted checks on the exit code
    of unit tests for as

--- scripts/unit_test_as.sh
+++ scripts/unit_test_as.sh
@@ -5,7 +5,17 @@ AS_DIR=$(realpath `git rev-parse --git-dir`/..)/assembler
 echo "build assembler"
 pushd $AS_DIR > /dev/null
 make test
+exit_code=$?
+if [[ $exit_code -ne 0 ]] ; then
+    echo "assembler build failed"
+    popd > /dev/null
+    exit $exit_code
+fi
+
 echo "assembler build successfull"
 popd > /dev/null
 echo "unit test assembler"
 $AS_DIR/build/test_assembler "$@"
+exit_code=$?
+
+exit $exit_code
