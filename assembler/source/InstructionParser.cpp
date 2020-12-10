2aff8f9cc136c7adfab39dc05d6d8ac5cb4e924e
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2020-12-10 17:44:32 +0100

    as/InstructionParser: Refactor if to check negative condition

--- assembler/source/InstructionParser.cpp
+++ assembler/source/InstructionParser.cpp
@@ -13,87 +13,85 @@ parsers::InstructionParser::InstructionParser(std::shared_ptr<assembler::SymbolT
 
 std::shared_ptr<statements::Statement> parsers::InstructionParser::parse(std::string statement)
 {
-    if (this->can_parse(statement))
+    if (!this->can_parse(statement))
+        return StatementParser::parse(statement);
+
+    const std::regex zero_operand_regex{ "^(halt"
+                                         "|iret"
+                                         "|ret)"
+                                         "\\s*$" };
+
+    const std::regex one_operand_regex{ "^((int"
+                                        "|call"
+                                        "|jmp"
+                                        "|jeq"
+                                        "|jne"
+                                        "|jgt"
+                                        "|push"
+                                        "|pop)"
+                                        "[bw]?)"
+                                        "\\s+([^,\\s]+)\\s*$" };
+
+    const std::regex two_operand_regex{ "^((xchg"
+                                        "|mov"
+                                        "|add"
+                                        "|sub"
+                                        "|mul"
+                                        "|div"
+                                        "|cmp"
+                                        "|not"
+                                        "|and"
+                                        "|or"
+                                        "|xor"
+                                        "|test"
+                                        "|shl"
+                                        "|shr)"
+                                        "[bw]?)"
+                                        "\\s+([^,\\s]+)\\s*,\\s*([^,\\s]+)\\s*$" };
+
+    std::smatch match;
+    if (std::regex_match(statement, match, zero_operand_regex))
     {
-        const std::regex zero_operand_regex{ "^(halt"
-                                             "|iret"
-                                             "|ret)"
-                                             "\\s*$" };
+        std::cout << "INSTRUCTION 0:'" << statement << "'" << match[1].str() << "\n";
 
-        const std::regex one_operand_regex{ "^((int"
-                                            "|call"
-                                            "|jmp"
-                                            "|jeq"
-                                            "|jne"
-                                            "|jgt"
-                                            "|push"
-                                            "|pop)"
-                                            "[bw]?)"
-                                            "\\s+([^,\\s]+)\\s*$" };
-
-        const std::regex two_operand_regex{ "^((xchg"
-                                            "|mov"
-                                            "|add"
-                                            "|sub"
-                                            "|mul"
-                                            "|div"
-                                            "|cmp"
-                                            "|not"
-                                            "|and"
-                                            "|or"
-                                            "|xor"
-                                            "|test"
-                                            "|shl"
-                                            "|shr)"
-                                            "[bw]?)"
-                                            "\\s+([^,\\s]+)\\s*,\\s*([^,\\s]+)\\s*$" };
-
-        std::smatch match;
-        if (std::regex_match(statement, match, zero_operand_regex))
-        {
-            std::cout << "INSTRUCTION 0:'" << statement << "'" << match[1].str() << "\n";
-
-            return nullptr;
-        }
-
-        if (std::regex_match(statement, match, one_operand_regex))
-        {
-            constexpr char DEFAULT_SIZE_OPERAND_SPECIFIER = 'w';
-
-            std::string mnemonic       = match[1].str();
-            std::string short_mnemonic = match[2].str();
-            if (short_mnemonic == mnemonic)
-            {
-                mnemonic = mnemonic + DEFAULT_SIZE_OPERAND_SPECIFIER;
-            }
-
-            char operand_size_specifier = mnemonic.back();
-            std::cout << "INSTRUCTION 1:'" << statement << "'" << mnemonic << "\n";
-
-            return nullptr;
-        }
-
-        if (std::regex_match(statement, match, two_operand_regex))
-        {
-            constexpr char DEFAULT_SIZE_OPERAND_SPECIFIER = 'w';
-
-            std::string mnemonic       = match[1].str();
-            std::string short_mnemonic = match[2].str();
-            if (short_mnemonic == mnemonic)
-            {
-                mnemonic = mnemonic + DEFAULT_SIZE_OPERAND_SPECIFIER;
-            }
-
-            char operand_size_specifier = mnemonic.back();
-            std::cout << "INSTRUCTION 2:'" << statement << "'" << mnemonic << "\n";
-
-            return nullptr;
-        }
-
-        throw std::invalid_argument{ "Invalid number of operands for instruction: " + statement };
+        return nullptr;
     }
 
-    return StatementParser::parse(statement);
+    if (std::regex_match(statement, match, one_operand_regex))
+    {
+        constexpr char DEFAULT_SIZE_OPERAND_SPECIFIER = 'w';
+
+        std::string mnemonic       = match[1].str();
+        std::string short_mnemonic = match[2].str();
+        if (short_mnemonic == mnemonic)
+        {
+            mnemonic = mnemonic + DEFAULT_SIZE_OPERAND_SPECIFIER;
+        }
+
+        char operand_size_specifier = mnemonic.back();
+        std::cout << "INSTRUCTION 1:'" << statement << "'" << mnemonic << "\n";
+
+        return nullptr;
+    }
+
+    if (std::regex_match(statement, match, two_operand_regex))
+    {
+        constexpr char DEFAULT_SIZE_OPERAND_SPECIFIER = 'w';
+
+        std::string mnemonic       = match[1].str();
+        std::string short_mnemonic = match[2].str();
+        if (short_mnemonic == mnemonic)
+        {
+            mnemonic = mnemonic + DEFAULT_SIZE_OPERAND_SPECIFIER;
+        }
+
+        char operand_size_specifier = mnemonic.back();
+        std::cout << "INSTRUCTION 2:'" << statement << "'" << mnemonic << "\n";
+
+        return nullptr;
+    }
+
+    throw std::invalid_argument{ "Invalid number of operands for instruction: " + statement };
 }
 
 bool parsers::InstructionParser::can_parse(const std::string &statement) const
