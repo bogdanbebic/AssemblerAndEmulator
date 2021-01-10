6a0fd31aa706b40cc7c6450775a81b1fe84a4849
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-01-10 14:15:08 +0100

    emu/Memory: Implement MMIO functions, refactor constants

--- emulator/include/Memory.hpp
+++ emulator/include/Memory.hpp
@@ -1,6 +1,10 @@
 #ifndef _MEMORY_HPP_
 #define _MEMORY_HPP_
 
+#include <memory>
+#include <vector>
+
+#include "MmioDevice.hpp"
 #include "Typedefs.hpp"
 
 namespace emulator
@@ -16,10 +20,17 @@ namespace emulator
             void write_word(mem_address_t base_address, word_t data);
             word_t read_word(mem_address_t base_address);
 
+            void add_mmio_device(std::shared_ptr<MmioDevice> device,
+                                 mem_address_t start_address,
+                                 mem_address_t end_address);
+
             enum
             {
-                MEMORY_SIZE     = mem_address_max + 1,
-                MMIO_SPACE_SIZE = 256,
+                MEMORY_SIZE = mem_address_max + 1,
+
+                MMIO_SPACE_SIZE    = 0x100,
+                MMIO_START_ADDRESS = 0xFF00,
+                MMIO_END_ADDRESS   = MMIO_START_ADDRESS + MMIO_SPACE_SIZE,
             };
 
         private:
@@ -27,24 +38,20 @@ namespace emulator
                 0,
             };
 
+            typedef struct MmioMapping
+            {
+                std::shared_ptr<MmioDevice> device;
+                mem_address_t start_address;
+                mem_address_t end_address;
+            } mmio_mapping_t;
+
             void write_mmio_byte(mem_address_t address, byte_t data);
             byte_t read_mmio_byte(mem_address_t address);
 
             void write_mmio_word(mem_address_t address, word_t data);
             word_t read_mmio_word(mem_address_t address);
 
-            enum MmioAdrresses
-            {
-                MMIO_START_ADDRESS = 0xFF00,
-
-                MMIO_TERMINAL_START = 0xFF00,
-                MMIO_TERMINAL_END   = MMIO_TERMINAL_START + 4,
-
-                MMIO_TIMER_START = 0xFF10,
-                MMIO_TIMER_END   = MMIO_TIMER_START + 2,
-
-                MMIO_END_ADDRESS = MMIO_START_ADDRESS + MMIO_SPACE_SIZE,
-            };
+            std::vector<mmio_mapping_t> mmio_devices;
         };
     } // namespace system
 } // namespace emulator
--- emulator/source/Memory.cpp
+++ emulator/source/Memory.cpp
@@ -56,24 +56,63 @@ emulator::system::word_t emulator::system::Memory::read_word(const mem_address_t
     return memory[base_address / sizeof(byte_t) * sizeof(word_t)];
 }
 
+void emulator::system::Memory::add_mmio_device(std::shared_ptr<MmioDevice> device,
+                                               mem_address_t start_address,
+                                               mem_address_t end_address)
+{
+    this->mmio_devices.push_back({ device, start_address, end_address });
+}
+
 void emulator::system::Memory::write_mmio_byte(mem_address_t address, byte_t data)
 {
-    // TODO: implementation
+    mem_address_t aligned_address = address & ~1;
+    word_t data_value             = this->read_mmio_word(aligned_address);
+
+    if (aligned_address == address)
+    {
+        data_value &= 0x00FF;
+        data_value &= data << 8;
+    }
+    else
+    {
+        data_value &= 0xFF00;
+        data_value &= data;
+    }
+
+    this->write_mmio_word(aligned_address, data_value);
 }
 
 emulator::system::byte_t emulator::system::Memory::read_mmio_byte(mem_address_t address)
 {
-    // TODO: implementation
-    return 0;
+    mem_address_t aligned_address = address & ~1;
+    word_t data_value             = this->read_mmio_word(aligned_address);
+
+    if (aligned_address == address)
+    {
+        data_value &= 0x00FF;
+    }
+    else
+    {
+        data_value &= 0xFF00;
+        data_value >>= 8;
+    }
+
+    return static_cast<byte_t>(data_value);
 }
 
 void emulator::system::Memory::write_mmio_word(mem_address_t address, word_t data)
 {
-    // TODO: implementation
+    for (auto &mmio_device : this->mmio_devices)
+        if (address >= mmio_device.start_address && address < mmio_device.end_address)
+            mmio_device.device->set_memory(address - mmio_device.start_address, data);
 }
 
 emulator::system::word_t emulator::system::Memory::read_mmio_word(mem_address_t address)
 {
-    // TODO: implementation
+    for (auto &mmio_device : this->mmio_devices)
+        if (address >= mmio_device.start_address && address < mmio_device.end_address)
+            return mmio_device.device->get_memory(address - mmio_device.start_address);
+
+    // no device mapped
     return 0;
 }
