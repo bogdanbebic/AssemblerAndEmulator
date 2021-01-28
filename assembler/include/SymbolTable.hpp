9e90b61ca99d700e329c5ad1371cc12b43728dd8
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-01-28 18:19:14 +0100

    as: Add SymbolTable::is_extern()

--- assembler/include/SymbolTable.hpp
+++ assembler/include/SymbolTable.hpp
@@ -31,6 +31,7 @@ namespace assembler
         void make_extern(const key_type &key);
         void make_declaration(const key_type &key);
         bool is_defined(const key_type &key);
+        bool is_extern(const key_type &key);
 
         std::stringstream to_school_elf() const;
 
