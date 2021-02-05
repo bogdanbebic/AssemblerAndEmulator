ceda55e0dd69bcafebaef0e4903c2d82b209a2c9
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-02-05 13:27:23 +0100

    tests/source_ss: Add hello_isr.s

---
+++ tests/source_ss/hello_isr.s
@@ -0,0 +1,42 @@
+.section only_section:
+# .section startup:
+
+.word _start
+.word _illegal_instruction_handler
+.word _timer_handler
+.word _terminal_handler
+.word interrupt_handler
+
+# .section isr_handlers:
+
+_illegal_instruction_handler:
+_timer_handler:
+_terminal_handler:
+    iret
+
+interrupt_handler:
+    mov (%r0), term_out
+    mov 1(%r0), term_out
+    mov 2(%r0), term_out
+    mov 3(%r0), term_out
+    mov 4(%r0), term_out
+    iret
+
+# .section text:
+
+_start:
+    mov $msg, %r0
+
+    int $4
+
+    halt
+
+# .section data:
+
+msg:
+.byte 'I', 'N', 'T', '4'
+.byte 10 # LF
+msg_end:
+
+.equ msg_len, msg_end - msg
+.equ term_out, 0xff00
