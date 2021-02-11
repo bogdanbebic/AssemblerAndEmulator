3fcc75699c9e58d552d3d4bf70ab570e9eeeae1d
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-02-11 14:02:29 +0100

    emu/ElfStructs: Add relocations vector to section_t

--- emulator/include/ElfStructs.hpp
+++ emulator/include/ElfStructs.hpp
@@ -25,12 +25,6 @@ namespace linker
             size_t size;
         } section_table_entry_t;
 
-        typedef struct Section
-        {
-            section_table_entry_t descriptor;
-            std::vector<emulator::system::byte_t> object_code;
-        } section_t;
-
         using relocation_type_t = int;
         typedef struct RelocationTableEntry
         {
@@ -38,6 +32,13 @@ namespace linker
             relocation_type_t type;
             size_t offset;
         } relocation_table_entry_t;
+
+        typedef struct Section
+        {
+            section_table_entry_t descriptor;
+            std::vector<emulator::system::byte_t> object_code;
+            std::vector<relocation_table_entry_t> relocations;
+        } section_t;
 
         symbol_table_entry_t parse_symbol_table_entry(std::string s);
         section_table_entry_t parse_section_table_entry(std::string s);
